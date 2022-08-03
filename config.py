5981325eafe7c8d585991db57cc76b82a25e20f5
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <93422036+MrAbhi2k3@users.noreply.github.com>
2022-08-03 10:13:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID")
 IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True preferable
 OWNER_ID = int(os.environ.get("OWNER_ID"))
 PROTECT_CONTENT = True
-UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
+UPDATE_GROUP = os.environ.get("UPDATE_GROUP", "")
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
 AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS") else []
 if OWNER_ID not in AUTH_USERS:
     AUTH_USERS.append(OWNER_ID)
